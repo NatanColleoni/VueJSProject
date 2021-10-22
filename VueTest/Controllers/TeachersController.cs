14d31a556c316010a9319bf79796dedfc701e83a
NatanColleoni <natancolleonifonseca@gmail.com>
2021-10-21 22:18:44 -0300

    Project Test

---
+++ VueTest/Controllers/TeachersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VueTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeachersController : Controller
+    {
+        public TeachersController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{TeacherId}")]
+        public IActionResult Get(int TeacherId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{TeacherId}")]
+        public IActionResult Put(int TeacherId)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{TeacherId}")]
+        public IActionResult Delete(int TeacherId)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ VueTest/Models/Teacher.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace VueTest.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
